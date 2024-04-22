beansrowning/HiTIN

commit a18ba9d8b0bef60245ce3523c7349659744a63a4

Moving back to config usage

data_modules/data_loader.py
@@ -22,7 +22,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
     collate_fn = Collator(config, vocab)
     train_dataset = ClassificationDataset(config, vocab, stage='TRAIN', on_memory=on_memory, corpus_lines=data['train'], tokenizer=tokenizer)
     train_loader = DataLoader(train_dataset,
-                              batch_size=config.batch_size,  # using args
+                              batch_size=config.train.batch_size,  # using args
                               shuffle=True,
                               num_workers=config.train.device_setting.num_workers,
                               collate_fn=collate_fn,
@@ -31,7 +31,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
 
     val_dataset = ClassificationDataset(config, vocab, stage='VAL', on_memory=on_memory, corpus_lines=data['val'], tokenizer=tokenizer)
     val_loader = DataLoader(val_dataset,
-                            batch_size=config.batch_size,  # using args
+                            batch_size=config.train.batch_size,  # using args
                             shuffle=True,
                             num_workers=config.train.device_setting.num_workers,
                             collate_fn=collate_fn,
@@ -40,7 +40,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
 
     test_dataset = ClassificationDataset(config, vocab, stage='TEST', on_memory=on_memory, corpus_lines=data['test'], tokenizer=tokenizer)
     test_loader = DataLoader(test_dataset,
-                             batch_size=config.batch_size,  # using args
+                             batch_size=config.train.batch_size,  # using args
                              shuffle=True,
                              num_workers=config.train.device_setting.num_workers,
                              collate_fn=collate_fn,

helper/logger.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import logging
-
+import time 
 logging_level = {'debug': logging.DEBUG,
                  'info': logging.INFO,
                  'warning': logging.WARNING,
@@ -49,13 +49,13 @@ class Logger(object):
 
         self.log_dir = os.path.join(config.log_dir,
                                config.model.type + '-' + config.structure_encoder.type + '-' + config.text_encoder.type,
-                               config.data.dataset + '_' + str(config.batch_size) + '_' + str(
-                                   config.learning_rate) + '_' + str(config.l2rate) + '_' + str(
-                                   config.classification_threshold) + '_' + str(config.hierar_penalty))
+                               config.data.dataset + '_' + str(config.train.batch_size) + '_' + str(
+                                   config.train.optimizer.learning_rate) + '_' + str(config.train.optimizer.lr_decay) + '_' + str(
+                                   config.eval.threshold) + '_' + str(config.train.loss.recursive_regularization.penalty))
         if config.structure_encoder.type == "TIN":
-            self.log_file = os.path.join(self.log_dir, config.begin_time + str(config.tree_depth) + '_' + str(config.hidden_dim) + '_' + config.tree_pooling_type + '_' + str(config.final_dropout) + '.log')
+            self.log_file = os.path.join(self.log_dir, time.strftime("%m%d_%H%M_", time.localtime()) + str(config.structure_encoder.tree_depth) + '_' + str(config.structure_encoder.hidden_dim) + '_' + config.structure_encoder.tree_pooling_type + '_' + str(config.structure_encoder.final_dropout) + '.log')
         else:
-            self.log_file = os.path.join(config.log_dir, config.begin_time + config.log.filename)
+            self.log_file = os.path.join(config.log_dir, time.strftime("%m%d_%H%M_", time.localtime()) + config.log.filename)
         if not os.path.isdir(self.log_dir):
             os.makedirs(self.log_dir)
 

models/structure_model/structure_encoder.py
@@ -62,14 +62,14 @@ class StructureEncoder(nn.Module):
                                                         in_matrix=self.node_prob_from_child,
                                                         out_matrix=self.node_prob_from_parent,
                                                         # in_dim=config.structure_encoder.node.dimension,
-                                                        in_dim=config.hidden_dim,
-                                                        hidden_dim=config.hidden_dim,
+                                                        in_dim=config.structure_encoder.hidden_dim,
+                                                        hidden_dim=config.structure_encoder.hidden_dim,
                                                         out_dim=len(self.label_map),
-                                                        dropout=config.final_dropout,
+                                                        dropout=config.structure_encoder.final_dropout,
                                                         device=device,
-                                                        depth=config.tree_depth,
-                                                        num_mlp_layers=config.num_mlp_layers,
-                                                        tree_pooling_type=config.tree_pooling_type)
+                                                        depth=config.structure_encoder.tree_depth,
+                                                        num_mlp_layers=config.structure_encoder.num_mlp_layers,
+                                                        tree_pooling_type=config.structure_encoder.tree_pooling_type)
 
         else:
             self.model = MODEL_MODULE[graph_model_type](num_nodes=len(self.label_map),

models/text_feature_propagation.py
@@ -30,7 +30,7 @@ class HiAGMTP(nn.Module):
         # Duplicate and project the text feature into the label embedding space.
         self.trans_dup = nn.Linear(len(self.label_map), len(self.label_map))
         self.trans_proj = nn.Linear(config.model.linear_transformation.text_dimension,
-                                    config.hidden_dim)
+                                    config.structure_encoder.hidden_dim)
         # classifier
         # self.linear = nn.Linear(len(self.label_map) * config.embedding.label.dimension,
         #                         len(self.label_map))  # original hiagm

train.py
@@ -33,10 +33,10 @@ def set_optimizer(config, model):
     """
     params = model.optimize_params_dict()
     if config.train.optimizer.type == 'Adam':
-        return torch.optim.Adam(lr=config.learning_rate,  # using args
+        return torch.optim.Adam(lr=config.train.optimizer.learning_rate,  # using args
             # lr=config.train.optimizer.learning_rate,
                                 params=params,
-                                weight_decay=config.l2rate)
+                                weight_decay=config.train.optimizer.lr_decay)
     else:
         raise TypeError("Recommend the Adam optimizer")
 
@@ -77,9 +77,9 @@ def train(config, args):
 
     # Define training objective & optimizer
     criterion = ClassificationLoss(os.path.join(config.data.data_dir, config.data.hierarchy),
-                                   corpus_vocab.v2i['doc_label'],
+                                   corpus_vocab.v2i['doc_label_list'],
                                    # recursive_penalty=config.train.loss.recursive_regularization.penalty,
-                                   recursive_penalty=args.hierar_penalty,  # using args
+                                   recursive_penalty=config.train.loss.recursive_regularization.penalty,  # using args
                                    recursive_constraint=config.train.loss.recursive_regularization.flag)
     if config.text_encoder.type == "bert":
         t_total = int(len(train_loader) * (config.train.end_epoch-config.train.start_epoch))
@@ -88,11 +88,11 @@ def train(config, args):
         no_decay = ['bias', 'LayerNorm.weight']
         grouped_parameters = [
             {'params': [p for n, p in param if 'bert' in n and not any(nd in n for nd in no_decay)],
-             'weight_decay': args.l2rate, 'lr': config.train.optimizer.learning_rate},
+             'weight_decay': config.train.optimizer.lr_decay, 'lr': config.train.optimizer.learning_rate},
             {'params': [p for n, p in param if 'bert' in n and any(nd in n for nd in no_decay)],
              'weight_decay': 0.0, 'lr': config.train.optimizer.learning_rate},
             {'params': [p for n, p in param if 'bert' not in n and not any(nd in n for nd in no_decay)],
-             'weight_decay': args.l2rate, 'lr': args.learning_rate},
+             'weight_decay': config.train.optimizer.lr_decay, 'lr': args.learning_rate},
             {'params': [p for n, p in param if 'bert' not in n and any(nd in n for nd in no_decay)],
              'weight_decay': 0.0, 'lr': args.learning_rate}
         ]
@@ -121,11 +121,10 @@ def train(config, args):
                 best_micro/macro-model_type-training_params_(tin_params)
                                             
     '''
-    # model_checkpoint = config.train.checkpoint.dir
-    model_checkpoint = os.path.join(args.ckpt_dir, args.begin_time + config.train.checkpoint.dir)  # using args
+    model_checkpoint = os.path.join(config.train.checkpoint.dir, time.strftime("%m%d_%H%M", time.localtime()))
     model_name = config.model.type
     if config.structure_encoder.type == "TIN":
-        model_name += '_' + str(args.tree_depth) + '_' + str(args.hidden_dim) + '_' + args.tree_pooling_type + '_' + str(args.final_dropout) + '_' + str(args.hierar_penalty)
+        model_name += '_' + str(config.structure_encoder.tree_depth) + '_' + str(config.structure_encoder.hidden_dim) + '_' + config.structure_encoder.tree_pooling_type + '_' + str(config.structure_encoder.final_dropout) + '_' + str(config.train.loss.recursive_regularization.penalty)
     wait = 0
     if not os.path.isdir(model_checkpoint):
         # os.mkdir(model_checkpoint)

train_modules/trainer.py
@@ -87,7 +87,7 @@ class Trainer(object):
                                target_labels,
                                self.vocab,
                                # self.config.eval.threshold
-                               self.config.classification_threshold)  # using args
+                               self.config.eval.threshold)  # using args
             # metrics = {'precision': precision_micro,
             #             'recall': recall_micro,
             #             'micro_f1': micro_f1,

data_modules/collator.py
@@ -14,7 +14,7 @@ class Collator(object):
         super(Collator, self).__init__()
         self.config = config
         self.device = config.train.device_setting.device
-        self.label_size = len(vocab.v2i['doc_label'].keys())
+        self.label_size = len(vocab.v2i['doc_label_list'].keys())
 
     def _multi_hot(self, batch_labels):
         """
@@ -52,7 +52,7 @@ class Collator(object):
 
         for sample in batch:
             batch_token.append(sample['doc_token'])
-            batch_label.append(sample['doc_label'])
+            batch_label.append(sample['doc_label_list'])
             batch_doc_len.append(sample['token_len'])
 
             if self.config.text_encoder.type == "bert":

data_modules/dataset.py
@@ -19,7 +19,7 @@ def get_sample_position(corpus_filename, on_memory, corpus_lines, stage):
     sample_position = [0]
     if not on_memory:
         print('Loading files for ' + stage + ' Dataset...')
-        with open(corpus_filename, 'r') as f_in:
+        with open(corpus_filename, 'r', encoding = "utf-8") as f_in:
             sample_str = f_in.readline()
             while sample_str:
                 sample_position.append(f_in.tell())
@@ -75,7 +75,7 @@ class ClassificationDataset(Dataset):
             raise IndexError
         if not self.on_memory:
             position = self.sample_position[index]
-            with open(self.corpus_file) as f_in:
+            with open(self.corpus_file, encoding="utf-8") as f_in:
                 f_in.seek(position)
                 sample_str = f_in.readline()
         else:
@@ -113,8 +113,8 @@ class ClassificationDataset(Dataset):
         :return: sample -> Dict{'token': List[int], 'label': List[int], 'token_len': int}
         """
         raw_sample = json.loads(sample_str)
-        sample = {'doc_token': [], 'doc_label': []}
-        for k in raw_sample.keys():
+        sample = {'doc_token': [], 'doc_label_list': []}
+        for k in sample.keys():
             if k == 'doc_token':
                 sample[k] = [self.vocab.v2i[k].get(v.lower(), self.vocab.oov_index) for v in raw_sample[k]]
 
@@ -133,9 +133,9 @@ class ClassificationDataset(Dataset):
         if not sample['doc_token']:
             sample['doc_token'].append(self.vocab.padding_index)
         if self.mode == 'TRAIN':
-            assert sample['doc_label'], 'Label is empty'
+            assert sample['doc_label_list'], 'Label is empty'
         else:
-            sample['doc_label'] = [0]
+            sample['doc_label_list'] = [0]
         sample['token_len'] = min(len(sample['doc_token']), self.max_input_length)
         padding = [self.vocab.padding_index for _ in range(0, self.max_input_length - len(sample['doc_token']))]
         sample['doc_token'] += padding

commit 6d7c17d28769473b18d37ddb619251f10b64969a

Undoing config changes - Something broke with a18ba9d8b0bef60245ce3523c7349659744a63a4

data_modules/data_loader.py
@@ -22,7 +22,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
     collate_fn = Collator(config, vocab)
     train_dataset = ClassificationDataset(config, vocab, stage='TRAIN', on_memory=on_memory, corpus_lines=data['train'], tokenizer=tokenizer)
     train_loader = DataLoader(train_dataset,
-                              batch_size=config.train.batch_size,  # using args
+                              batch_size=config.batch_size,  # using args
                               shuffle=True,
                               num_workers=config.train.device_setting.num_workers,
                               collate_fn=collate_fn,
@@ -31,7 +31,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
 
     val_dataset = ClassificationDataset(config, vocab, stage='VAL', on_memory=on_memory, corpus_lines=data['val'], tokenizer=tokenizer)
     val_loader = DataLoader(val_dataset,
-                            batch_size=config.train.batch_size,  # using args
+                            batch_size=config.batch_size,  # using args
                             shuffle=True,
                             num_workers=config.train.device_setting.num_workers,
                             collate_fn=collate_fn,
@@ -40,7 +40,7 @@ def data_loaders(config, vocab, data={'train': None, 'val': None, 'test': None},
 
     test_dataset = ClassificationDataset(config, vocab, stage='TEST', on_memory=on_memory, corpus_lines=data['test'], tokenizer=tokenizer)
     test_loader = DataLoader(test_dataset,
-                             batch_size=config.train.batch_size,  # using args
+                             batch_size=config.batch_size,  # using args
                              shuffle=True,
                              num_workers=config.train.device_setting.num_workers,
                              collate_fn=collate_fn,

helper/logger.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import logging
-import time 
+
 logging_level = {'debug': logging.DEBUG,
                  'info': logging.INFO,
                  'warning': logging.WARNING,
@@ -49,13 +49,13 @@ class Logger(object):
 
         self.log_dir = os.path.join(config.log_dir,
                                config.model.type + '-' + config.structure_encoder.type + '-' + config.text_encoder.type,
-                               config.data.dataset + '_' + str(config.train.batch_size) + '_' + str(
-                                   config.train.optimizer.learning_rate) + '_' + str(config.train.optimizer.lr_decay) + '_' + str(
-                                   config.eval.threshold) + '_' + str(config.train.loss.recursive_regularization.penalty))
+                               config.data.dataset + '_' + str(config.batch_size) + '_' + str(
+                                   config.learning_rate) + '_' + str(config.l2rate) + '_' + str(
+                                   config.classification_threshold) + '_' + str(config.hierar_penalty))
         if config.structure_encoder.type == "TIN":
-            self.log_file = os.path.join(self.log_dir, time.strftime("%m%d_%H%M_", time.localtime()) + str(config.structure_encoder.tree_depth) + '_' + str(config.structure_encoder.hidden_dim) + '_' + config.structure_encoder.tree_pooling_type + '_' + str(config.structure_encoder.final_dropout) + '.log')
+            self.log_file = os.path.join(self.log_dir, config.begin_time + str(config.tree_depth) + '_' + str(config.hidden_dim) + '_' + config.tree_pooling_type + '_' + str(config.final_dropout) + '.log')
         else:
-            self.log_file = os.path.join(config.log_dir, time.strftime("%m%d_%H%M_", time.localtime()) + config.log.filename)
+            self.log_file = os.path.join(config.log_dir, config.begin_time + config.log.filename)
         if not os.path.isdir(self.log_dir):
             os.makedirs(self.log_dir)
 

models/structure_model/structure_encoder.py
@@ -62,14 +62,14 @@ class StructureEncoder(nn.Module):
                                                         in_matrix=self.node_prob_from_child,
                                                         out_matrix=self.node_prob_from_parent,
                                                         # in_dim=config.structure_encoder.node.dimension,
-                                                        in_dim=config.structure_encoder.hidden_dim,
-                                                        hidden_dim=config.structure_encoder.hidden_dim,
+                                                        in_dim=config.hidden_dim,
+                                                        hidden_dim=config.hidden_dim,
                                                         out_dim=len(self.label_map),
-                                                        dropout=config.structure_encoder.final_dropout,
+                                                        dropout=config.final_dropout,
                                                         device=device,
-                                                        depth=config.structure_encoder.tree_depth,
-                                                        num_mlp_layers=config.structure_encoder.num_mlp_layers,
-                                                        tree_pooling_type=config.structure_encoder.tree_pooling_type)
+                                                        depth=config.tree_depth,
+                                                        num_mlp_layers=config.num_mlp_layers,
+                                                        tree_pooling_type=config.tree_pooling_type)
 
         else:
             self.model = MODEL_MODULE[graph_model_type](num_nodes=len(self.label_map),

models/text_feature_propagation.py
@@ -30,7 +30,7 @@ class HiAGMTP(nn.Module):
         # Duplicate and project the text feature into the label embedding space.
         self.trans_dup = nn.Linear(len(self.label_map), len(self.label_map))
         self.trans_proj = nn.Linear(config.model.linear_transformation.text_dimension,
-                                    config.structure_encoder.hidden_dim)
+                                    config.hidden_dim)
         # classifier
         # self.linear = nn.Linear(len(self.label_map) * config.embedding.label.dimension,
         #                         len(self.label_map))  # original hiagm

train.py
@@ -33,10 +33,10 @@ def set_optimizer(config, model):
     """
     params = model.optimize_params_dict()
     if config.train.optimizer.type == 'Adam':
-        return torch.optim.Adam(lr=config.train.optimizer.learning_rate,  # using args
+        return torch.optim.Adam(lr=config.learning_rate,  # using args
             # lr=config.train.optimizer.learning_rate,
                                 params=params,
-                                weight_decay=config.train.optimizer.lr_decay)
+                                weight_decay=config.l2rate)
     else:
         raise TypeError("Recommend the Adam optimizer")
 
@@ -77,9 +77,9 @@ def train(config, args):
 
     # Define training objective & optimizer
     criterion = ClassificationLoss(os.path.join(config.data.data_dir, config.data.hierarchy),
-                                   corpus_vocab.v2i['doc_label_list'],
+                                   corpus_vocab.v2i['doc_label'],
                                    # recursive_penalty=config.train.loss.recursive_regularization.penalty,
-                                   recursive_penalty=config.train.loss.recursive_regularization.penalty,  # using args
+                                   recursive_penalty=args.hierar_penalty,  # using args
                                    recursive_constraint=config.train.loss.recursive_regularization.flag)
     if config.text_encoder.type == "bert":
         t_total = int(len(train_loader) * (config.train.end_epoch-config.train.start_epoch))
@@ -88,11 +88,11 @@ def train(config, args):
         no_decay = ['bias', 'LayerNorm.weight']
         grouped_parameters = [
             {'params': [p for n, p in param if 'bert' in n and not any(nd in n for nd in no_decay)],
-             'weight_decay': config.train.optimizer.lr_decay, 'lr': config.train.optimizer.learning_rate},
+             'weight_decay': args.l2rate, 'lr': config.train.optimizer.learning_rate},
             {'params': [p for n, p in param if 'bert' in n and any(nd in n for nd in no_decay)],
              'weight_decay': 0.0, 'lr': config.train.optimizer.learning_rate},
             {'params': [p for n, p in param if 'bert' not in n and not any(nd in n for nd in no_decay)],
-             'weight_decay': config.train.optimizer.lr_decay, 'lr': args.learning_rate},
+             'weight_decay': args.l2rate, 'lr': args.learning_rate},
             {'params': [p for n, p in param if 'bert' not in n and any(nd in n for nd in no_decay)],
              'weight_decay': 0.0, 'lr': args.learning_rate}
         ]
@@ -121,10 +121,11 @@ def train(config, args):
                 best_micro/macro-model_type-training_params_(tin_params)
                                             
     '''
-    model_checkpoint = os.path.join(config.train.checkpoint.dir, time.strftime("%m%d_%H%M", time.localtime()))
+    # model_checkpoint = config.train.checkpoint.dir
+    model_checkpoint = os.path.join(args.ckpt_dir, args.begin_time + config.train.checkpoint.dir)  # using args
     model_name = config.model.type
     if config.structure_encoder.type == "TIN":
-        model_name += '_' + str(config.structure_encoder.tree_depth) + '_' + str(config.structure_encoder.hidden_dim) + '_' + config.structure_encoder.tree_pooling_type + '_' + str(config.structure_encoder.final_dropout) + '_' + str(config.train.loss.recursive_regularization.penalty)
+        model_name += '_' + str(args.tree_depth) + '_' + str(args.hidden_dim) + '_' + args.tree_pooling_type + '_' + str(args.final_dropout) + '_' + str(args.hierar_penalty)
     wait = 0
     if not os.path.isdir(model_checkpoint):
         # os.mkdir(model_checkpoint)

train_modules/trainer.py
@@ -87,7 +87,7 @@ class Trainer(object):
                                target_labels,
                                self.vocab,
                                # self.config.eval.threshold
-                               self.config.eval.threshold)  # using args
+                               self.config.classification_threshold)  # using args
             # metrics = {'precision': precision_micro,
             #             'recall': recall_micro,
             #             'micro_f1': micro_f1,