e2aabfd2369ca1c335fe6966c3bc13803773193d
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2024-08-20 17:14:21 -0400

    Adding deps

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='HiTIN',
     version='0.1',
     description='A packaged version of the HiTIN model implemented by Zhu et al.',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     python_requires=">=3.9",
     url='https://github.com/beansrowning/HiTIN',
     author='Sean Browning',
     author_email='sbrowning@cdc.gov',
     license='MIT',
     packages=find_packages(include=["hi_tin", "hi_tin.*"]),
+    install_requires=requirements,
     zip_safe=False
 )
